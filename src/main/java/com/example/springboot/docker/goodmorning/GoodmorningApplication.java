b6035c9930e022764ea7f849a98793cb065ffabf
Kishore <ktallapakam@gmail.com>
2025-04-02 18:05:19 +0530

    application modified

--- src/main/java/com/example/springboot/docker/goodmorning/GoodmorningApplication.java
+++ src/main/java/com/example/springboot/docker/goodmorning/GoodmorningApplication.java
@@ -3,20 +3,20 @@ package com.example.springboot.docker.goodmorning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class GoodmorningApplication {
 
-	@GetMapping("/message")
-	public String message() {
+	@GetMapping("/hello/{message}")
+	public String message(@PathVariable("message") String name) {
 		System.out.println("Good morning....message()");
-		return "<h1>Hello World .....!</h1>" ;
+		return "<h1>Hello Mr/Mrs."+name+".....Good morning!</h1>" ;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Good morning....main()");
 		SpringApplication.run(GoodmorningApplication.class, args);
 	}
 
